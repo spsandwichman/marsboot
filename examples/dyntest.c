4fb5873c03f04a049c2defb0bd8fd230e03f8fc8
sandwich <sethpoulsen@gmail.com>
2025-02-23 10:18:54 -0500

    obliterate emitjkl

--- examples/dyntest.c
+++ examples/dyntest.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
 
-typedef void* Ptr;
+typedef uint8_t* Ptr;
 typedef struct Slice { Ptr raw; uint64_t len; } Slice;
 typedef struct Dyn { Ptr raw; uint64_t id; } Dyn;
-typedef void (*Fun_55ec8675bb60)(Dyn);
-typedef void (*Fun_55ec8675be70)();
+typedef void (*Fun_236724e9080)(Dyn);
+typedef void (*Fun_236724e93c0)();
 
 void dyntest_pass_dyn(Dyn x);
 void dyntest_main();
@@ -13,11 +13,11 @@ void dyntest_pass_dyn(Dyn x) {
 }
 
 void dyntest_main() {
-  int64_t v55ec8675bfd0 = (int64_t)1ll;
-  int64_t x = v55ec8675bfd0;
-  int64_t v55ec8675c0b0 = x;
-  Dyn v55ec8675c0f0 = (Dyn)v55ec8675c0b0;
-  Fun_55ec8675bb60 v55ec8675c070 = dyntest_pass_dyn;
-  void v55ec8675c010 = v55ec8675c070(v55ec8675c0f0);
+  int64_t v236724e8f40 = (int64_t)1ll;
+  int64_t x = v236724e8f40;
+  int64_t v236724e9180 = x;
+  Dyn v236724e8fc0 = (Dyn)v236724e9180;
+  Fun_236724e9080 v236724e9100 = dyntest_pass_dyn;
+  void v236724e9440 = v236724e9100(v236724e8fc0);
 }
 
--- examples/vec3.c
+++ examples/vec3.c
@@ -1,187 +1,187 @@
 #include <stdint.h>
 
-typedef void* Ptr;
+typedef uint8_t* Ptr;
 typedef struct Slice { Ptr raw; uint64_t len; } Slice;
 typedef struct Dyn { Ptr raw; uint64_t id; } Dyn;
-typedef struct Struct_2672449e4e0 {
+typedef struct Struct_199b031e920 {
   float x;
   float y;
   float z;
-} Struct_2672449e4e0;
-typedef Struct_2672449e4e0 (*Fun_267244a18e0)(Struct_2672449e4e0);
-typedef Struct_2672449e4e0 (*Fun_267244a1aa0)(Struct_2672449e4e0);
-typedef Struct_2672449e4e0 (*Fun_267244a1820)(Struct_2672449e4e0, Struct_2672449e4e0);
-typedef Struct_2672449e4e0 (*Fun_267244a34c0)(Struct_2672449e4e0, Struct_2672449e4e0);
-typedef Struct_2672449e4e0 (*Fun_267244a2900)(Struct_2672449e4e0, Struct_2672449e4e0);
-typedef Struct_2672449e4e0 (*Fun_267244a2b40)(Struct_2672449e4e0, Struct_2672449e4e0);
-typedef Struct_2672449e4e0 (*Fun_267244a5310)(Slice);
+} Struct_199b031e920;
+typedef Struct_199b031e920 (*Fun_199b0321160)(Struct_199b031e920);
+typedef Struct_199b031e920 (*Fun_199b03213e0)(Struct_199b031e920);
+typedef Struct_199b031e920 (*Fun_199b0320ea0)(Struct_199b031e920, Struct_199b031e920);
+typedef Struct_199b031e920 (*Fun_199b0322940)(Struct_199b031e920, Struct_199b031e920);
+typedef Struct_199b031e920 (*Fun_199b0323340)(Struct_199b031e920, Struct_199b031e920);
+typedef Struct_199b031e920 (*Fun_199b0323080)(Struct_199b031e920, Struct_199b031e920);
+typedef Struct_199b031e920 (*Fun_199b03259d0)(Slice);
 
-Struct_2672449e4e0 vec3_neg(Struct_2672449e4e0 a);
-Struct_2672449e4e0 vec3_inv(Struct_2672449e4e0 a);
-Struct_2672449e4e0 vec3_add(Struct_2672449e4e0 a, Struct_2672449e4e0 b);
-Struct_2672449e4e0 vec3_sub(Struct_2672449e4e0 a, Struct_2672449e4e0 b);
-Struct_2672449e4e0 vec3_mul(Struct_2672449e4e0 a, Struct_2672449e4e0 b);
-Struct_2672449e4e0 vec3_div(Struct_2672449e4e0 a, Struct_2672449e4e0 b);
-Struct_2672449e4e0 vec3_sum_all(Slice vecs);
+Struct_199b031e920 vec3_neg(Struct_199b031e920 a);
+Struct_199b031e920 vec3_inv(Struct_199b031e920 a);
+Struct_199b031e920 vec3_add(Struct_199b031e920 a, Struct_199b031e920 b);
+Struct_199b031e920 vec3_sub(Struct_199b031e920 a, Struct_199b031e920 b);
+Struct_199b031e920 vec3_mul(Struct_199b031e920 a, Struct_199b031e920 b);
+Struct_199b031e920 vec3_div(Struct_199b031e920 a, Struct_199b031e920 b);
+Struct_199b031e920 vec3_sum_all(Slice vecs);
 
-Struct_2672449e4e0 vec3_neg(Struct_2672449e4e0 a) {
-  Ptr v267244a1420 = &a;
-  float v267244a12e0 = ((Struct_2672449e4e0*)v267244a1420)->x;
-  float v267244a1360 = -v267244a12e0;
-  Ptr v267244a1ce0 = &a;
-  float v267244a1a20 = ((Struct_2672449e4e0*)v267244a1ce0)->y;
-  float v267244a19a0 = -v267244a1a20;
-  Ptr v267244a1320 = &a;
-  float v267244a1be0 = ((Struct_2672449e4e0*)v267244a1320)->z;
-  float v267244a1ca0 = -v267244a1be0;
-  Struct_2672449e4e0 v267244a19e0 = {v267244a1360, v267244a19a0, v267244a1ca0};
-  return v267244a19e0;
+Struct_199b031e920 vec3_neg(Struct_199b031e920 a) {
+  Ptr v199b0320f60 = &a;
+  float v199b0320fe0 = ((Struct_199b031e920*)v199b0320f60)->x;
+  float v199b0321060 = -v199b0320fe0;
+  Ptr v199b03219e0 = &a;
+  float v199b0321020 = ((Struct_199b031e920*)v199b03219e0)->y;
+  float v199b03218a0 = -v199b0321020;
+  Ptr v199b03210a0 = &a;
+  float v199b0321ae0 = ((Struct_199b031e920*)v199b03210a0)->z;
+  float v199b0320ee0 = -v199b0321ae0;
+  Struct_199b031e920 v199b03215a0 = {v199b0321060, v199b03218a0, v199b0320ee0};
+  return v199b03215a0;
 }
 
-Struct_2672449e4e0 vec3_inv(Struct_2672449e4e0 a) {
-  float v267244a11a0 = (float)1.000000;
-  Ptr v267244a1b20 = &a;
-  float v267244a1220 = ((Struct_2672449e4e0*)v267244a1b20)->x;
-  float v267244a1160 = v267244a11a0 / v267244a1220;
-  float v267244a16a0 = (float)1.000000;
-  Ptr v267244a1120 = &a;
-  float v267244a17a0 = ((Struct_2672449e4e0*)v267244a1120)->y;
-  float v267244a15e0 = v267244a16a0 / v267244a17a0;
-  float v267244a1460 = (float)1.000000;
-  Ptr v267244a14a0 = &a;
-  float v267244a17e0 = ((Struct_2672449e4e0*)v267244a14a0)->z;
-  float v267244a1b60 = v267244a1460 / v267244a17e0;
-  Struct_2672449e4e0 v267244a1c20 = {v267244a1160, v267244a15e0, v267244a1b60};
-  return v267244a1c20;
+Struct_199b031e920 vec3_inv(Struct_199b031e920 a) {
+  float v199b0321860 = (float)1.000000;
+  Ptr v199b03214a0 = &a;
+  float v199b03215e0 = ((Struct_199b031e920*)v199b03214a0)->x;
+  float v199b0321960 = v199b0321860 / v199b03215e0;
+  float v199b0321a60 = (float)1.000000;
+  Ptr v199b03218e0 = &a;
+  float v199b0321120 = ((Struct_199b031e920*)v199b03218e0)->y;
+  float v199b0321c60 = v199b0321a60 / v199b0321120;
+  float v199b0321b60 = (float)1.000000;
+  Ptr v199b0321ba0 = &a;
+  float v199b03214e0 = ((Struct_199b031e920*)v199b0321ba0)->z;
+  float v199b0321360 = v199b0321b60 / v199b03214e0;
+  Struct_199b031e920 v199b0321760 = {v199b0321960, v199b0321c60, v199b0321360};
+  return v199b0321760;
 }
 
-Struct_2672449e4e0 vec3_add(Struct_2672449e4e0 a, Struct_2672449e4e0 b) {
-  Ptr v267244a0f60 = &a;
-  float v267244a0f20 = ((Struct_2672449e4e0*)v267244a0f60)->x;
-  Ptr v267244a14e0 = &b;
-  float v267244a1860 = ((Struct_2672449e4e0*)v267244a14e0)->x;
-  float v267244a1960 = v267244a0f20 + v267244a1860;
-  Ptr v267244a1260 = &a;
-  float v267244a1520 = ((Struct_2672449e4e0*)v267244a1260)->y;
-  Ptr v267244a13e0 = &b;
-  float v267244a1560 = ((Struct_2672449e4e0*)v267244a13e0)->y;
-  float v267244a0e60 = v267244a1520 + v267244a1560;
-  Ptr v267244a0ea0 = &a;
-  float v267244a15a0 = ((Struct_2672449e4e0*)v267244a0ea0)->z;
-  Ptr v267244a0fe0 = &b;
-  float v267244a0ee0 = ((Struct_2672449e4e0*)v267244a0fe0)->z;
-  float v267244a1020 = v267244a15a0 + v267244a0ee0;
-  Struct_2672449e4e0 v267244a13a0 = {v267244a1960, v267244a0e60, v267244a1020};
-  return v267244a13a0;
+Struct_199b031e920 vec3_add(Struct_199b031e920 a, Struct_199b031e920 b) {
+  Ptr v199b0321ce0 = &a;
+  float v199b0320f20 = ((Struct_199b031e920*)v199b0321ce0)->x;
+  Ptr v199b03217e0 = &b;
+  float v199b0321520 = ((Struct_199b031e920*)v199b03217e0)->x;
+  float v199b0321560 = v199b0320f20 + v199b0321520;
+  Ptr v199b03211a0 = &a;
+  float v199b0321d20 = ((Struct_199b031e920*)v199b03211a0)->y;
+  Ptr v199b0321660 = &b;
+  float v199b03211e0 = ((Struct_199b031e920*)v199b0321660)->y;
+  float v199b0321d60 = v199b0321d20 + v199b03211e0;
+  Ptr v199b0321260 = &a;
+  float v199b0321220 = ((Struct_199b031e920*)v199b0321260)->z;
+  Ptr v199b0321320 = &b;
+  float v199b03212a0 = ((Struct_199b031e920*)v199b0321320)->z;
+  float v199b03216a0 = v199b0321220 + v199b03212a0;
+  Struct_199b031e920 v199b0321c20 = {v199b0321560, v199b0321d60, v199b03216a0};
+  return v199b0321c20;
 }
 
-Struct_2672449e4e0 vec3_sub(Struct_2672449e4e0 a, Struct_2672449e4e0 b) {
-  Ptr v267244a33c0 = &a;
-  float v267244a2c40 = ((Struct_2672449e4e0*)v267244a33c0)->x;
-  Ptr v267244a3400 = &b;
-  float v267244a2c00 = ((Struct_2672449e4e0*)v267244a3400)->x;
-  float v267244a2dc0 = v267244a2c40 - v267244a2c00;
-  Ptr v267244a2a80 = &a;
-  float v267244a3440 = ((Struct_2672449e4e0*)v267244a2a80)->y;
-  Ptr v267244a3500 = &b;
-  float v267244a2cc0 = ((Struct_2672449e4e0*)v267244a3500)->y;
-  float v267244a2e40 = v267244a3440 - v267244a2cc0;
-  Ptr v267244a26c0 = &a;
-  float v267244a2700 = ((Struct_2672449e4e0*)v267244a26c0)->z;
-  Ptr v267244a2e80 = &b;
-  float v267244a2740 = ((Struct_2672449e4e0*)v267244a2e80)->z;
-  float v267244a2940 = v267244a2700 - v267244a2740;
-  Struct_2672449e4e0 v267244a3380 = {v267244a2dc0, v267244a2e40, v267244a2940};
-  return v267244a3380;
+Struct_199b031e920 vec3_sub(Struct_199b031e920 a, Struct_199b031e920 b) {
+  Ptr v199b0323400 = &a;
+  float v199b0322980 = ((Struct_199b031e920*)v199b0323400)->x;
+  Ptr v199b0323300 = &b;
+  float v199b0322c40 = ((Struct_199b031e920*)v199b0323300)->x;
+  float v199b0322b40 = v199b0322980 - v199b0322c40;
+  Ptr v199b0322f80 = &a;
+  float v199b0322f40 = ((Struct_199b031e920*)v199b0322f80)->y;
+  Ptr v199b0322fc0 = &b;
+  float v199b0322ec0 = ((Struct_199b031e920*)v199b0322fc0)->y;
+  float v199b0322dc0 = v199b0322f40 - v199b0322ec0;
+  Ptr v199b0323480 = &a;
+  float v199b0322c80 = ((Struct_199b031e920*)v199b0323480)->z;
+  Ptr v199b0322bc0 = &b;
+  float v199b0322b80 = ((Struct_199b031e920*)v199b0322bc0)->z;
+  float v199b0322a00 = v199b0322c80 - v199b0322b80;
+  Struct_199b031e920 v199b0322b00 = {v199b0322b40, v199b0322dc0, v199b0322a00};
+  return v199b0322b00;
 }
 
-Struct_2672449e4e0 vec3_mul(Struct_2672449e4e0 a, Struct_2672449e4e0 b) {
-  Ptr v267244a2d40 = &a;
-  float v267244a2d00 = ((Struct_2672449e4e0*)v267244a2d40)->x;
-  Ptr v267244a2ec0 = &b;
-  float v267244a25c0 = ((Struct_2672449e4e0*)v267244a2ec0)->x;
-  float v267244a2600 = v267244a2d00 * v267244a25c0;
-  Ptr v267244a2a00 = &a;
-  float v267244a3280 = ((Struct_2672449e4e0*)v267244a2a00)->y;
-  Ptr v267244a3240 = &b;
-  float v267244a2780 = ((Struct_2672449e4e0*)v267244a3240)->y;
-  float v267244a3200 = v267244a3280 * v267244a2780;
-  Ptr v267244a27c0 = &a;
-  float v267244a2fc0 = ((Struct_2672449e4e0*)v267244a27c0)->z;
-  Ptr v267244a2640 = &b;
-  float v267244a2d80 = ((Struct_2672449e4e0*)v267244a2640)->z;
-  float v267244a2f40 = v267244a2fc0 * v267244a2d80;
-  Struct_2672449e4e0 v267244a2f00 = {v267244a2600, v267244a3200, v267244a2f40};
-  return v267244a2f00;
+Struct_199b031e920 vec3_mul(Struct_199b031e920 a, Struct_199b031e920 b) {
+  Ptr v199b0322d40 = &a;
+  float v199b0322800 = ((Struct_199b031e920*)v199b0322d40)->x;
+  Ptr v199b0322f00 = &b;
+  float v199b0322d80 = ((Struct_199b031e920*)v199b0322f00)->x;
+  float v199b0322700 = v199b0322800 * v199b0322d80;
+  Ptr v199b0322e40 = &a;
+  float v199b0322e00 = ((Struct_199b031e920*)v199b0322e40)->y;
+  Ptr v199b0322e80 = &b;
+  float v199b0323200 = ((Struct_199b031e920*)v199b0322e80)->y;
+  float v199b03225c0 = v199b0322e00 * v199b0323200;
+  Ptr v199b0322840 = &a;
+  float v199b0323380 = ((Struct_199b031e920*)v199b0322840)->z;
+  Ptr v199b0323440 = &b;
+  float v199b0323500 = ((Struct_199b031e920*)v199b0323440)->z;
+  float v199b0322640 = v199b0323380 * v199b0323500;
+  Struct_199b031e920 v199b03231c0 = {v199b0322700, v199b03225c0, v199b0322640};
+  return v199b03231c0;
 }
 
-Struct_2672449e4e0 vec3_div(Struct_2672449e4e0 a, Struct_2672449e4e0 b) {
-  Ptr v267244a2a40 = &a;
-  float v267244a2840 = ((Struct_2672449e4e0*)v267244a2a40)->x;
-  Ptr v267244a2ac0 = &b;
-  float v267244a28c0 = ((Struct_2672449e4e0*)v267244a2ac0)->x;
-  float v267244a2b80 = v267244a2840 / v267244a28c0;
-  Ptr v267244a2e00 = &a;
-  float v267244a2bc0 = ((Struct_2672449e4e0*)v267244a2e00)->y;
-  Ptr v267244a2f80 = &b;
-  float v267244a3000 = ((Struct_2672449e4e0*)v267244a2f80)->y;
-  float v267244a3040 = v267244a2bc0 / v267244a3000;
-  Ptr v267244a30c0 = &a;
-  float v267244a3080 = ((Struct_2672449e4e0*)v267244a30c0)->z;
-  Ptr v267244a3180 = &b;
-  float v267244a3100 = ((Struct_2672449e4e0*)v267244a3180)->z;
-  float v267244a5090 = v267244a3080 / v267244a3100;
-  Struct_2672449e4e0 v267244a2680 = {v267244a2b80, v267244a3040, v267244a5090};
-  return v267244a2680;
+Struct_199b031e920 vec3_div(Struct_199b031e920 a, Struct_199b031e920 b) {
+  Ptr v199b03228c0 = &a;
+  float v199b0322740 = ((Struct_199b031e920*)v199b03228c0)->x;
+  Ptr v199b0323240 = &b;
+  float v199b0323140 = ((Struct_199b031e920*)v199b0323240)->x;
+  float v199b0323280 = v199b0322740 / v199b0323140;
+  Ptr v199b03233c0 = &a;
+  float v199b03232c0 = ((Struct_199b031e920*)v199b03233c0)->y;
+  Ptr v199b0322780 = &b;
+  float v199b0322880 = ((Struct_199b031e920*)v199b0322780)->y;
+  float v199b03227c0 = v199b03232c0 / v199b0322880;
+  Ptr v199b03229c0 = &a;
+  float v199b0322900 = ((Struct_199b031e920*)v199b03229c0)->z;
+  Ptr v199b0322ac0 = &b;
+  float v199b0322a80 = ((Struct_199b031e920*)v199b0322ac0)->z;
+  float v199b0325c50 = v199b0322900 / v199b0322a80;
+  Struct_199b031e920 v199b03226c0 = {v199b0323280, v199b03227c0, v199b0325c50};
+  return v199b03226c0;
 }
 
-Struct_2672449e4e0 vec3_sum_all(Slice vecs) {
-  Struct_2672449e4e0 v267244a5150 = (Struct_2672449e4e0){0};
-  Struct_2672449e4e0 sum = v267244a5150;
+Struct_199b031e920 vec3_sum_all(Slice vecs) {
+  Struct_199b031e920 v199b0325cd0 = (Struct_199b031e920){0};
+  Struct_199b031e920 sum = v199b0325cd0;
   {
-    uint64_t v267244a5e90 = (uint64_t)0llu;
-    uint64_t i = v267244a5e90;
+    uint64_t v199b0325710 = (uint64_t)0llu;
+    uint64_t i = v199b0325710;
     while (1) {
-      uint64_t v267244a6b60 = i;
-      Slice v267244a5e10 = vecs;
-      uint64_t v267244a5590 = v267244a5e10.len;
-      if (!(v267244a6b60 < v267244a5590)) break;
+      uint64_t v199b03268e0 = i;
+      Slice v199b0325a50 = vecs;
+      uint64_t v199b0325b50 = v199b0325a50.len;
+      if (!(v199b03268e0 < v199b0325b50)) break;
       {
-        Ptr v267244a58d0 = &sum;
-        float v267244a5890 = ((Struct_2672449e4e0*)v267244a58d0)->x;
-        uint64_t v267244a5190 = i;
-        Slice v267244a4fd0 = vecs;
-        Ptr v267244a59d0 = &((Struct_2672449e4e0*)v267244a4fd0.raw)[v267244a5190];
-        float v267244a5550 = ((Struct_2672449e4e0*)v267244a59d0)->x;
-        float v267244a4f90 = v267244a5890 + v267244a5550;
-        Ptr v267244a5c50 = &sum;
-        Ptr v267244a5710 = &((Struct_2672449e4e0*)v267244a5c50)->x;
-        *(float*)v267244a5710 = v267244a4f90;
-        Ptr v267244a5010 = &sum;
-        float v267244a5350 = ((Struct_2672449e4e0*)v267244a5010)->y;
-        uint64_t v267244a5050 = i;
-        Slice v267244a53d0 = vecs;
-        Ptr v267244a5390 = &((Struct_2672449e4e0*)v267244a53d0.raw)[v267244a5050];
-        float v267244a5e50 = ((Struct_2672449e4e0*)v267244a5390)->y;
-        float v267244a5410 = v267244a5350 + v267244a5e50;
-        Ptr v267244a5810 = &sum;
-        Ptr v267244a50d0 = &((Struct_2672449e4e0*)v267244a5810)->y;
-        *(float*)v267244a50d0 = v267244a5410;
-        Ptr v267244a5a10 = &sum;
-        float v267244a51d0 = ((Struct_2672449e4e0*)v267244a5a10)->z;
-        uint64_t v267244a5250 = i;
-        Slice v267244a5a50 = vecs;
-        Ptr v267244a5790 = &((Struct_2672449e4e0*)v267244a5a50.raw)[v267244a5250];
-        float v267244a5210 = ((Struct_2672449e4e0*)v267244a5790)->z;
-        float v267244a5b10 = v267244a51d0 + v267244a5210;
-        Ptr v267244a54d0 = &sum;
-        Ptr v267244a5490 = &((Struct_2672449e4e0*)v267244a54d0)->z;
-        *(float*)v267244a5490 = v267244a5b10;
+        Ptr v199b0325690 = &sum;
+        float v199b0325e90 = ((Struct_199b031e920*)v199b0325690)->x;
+        uint64_t v199b0325b90 = i;
+        Slice v199b0325790 = vecs;
+        Ptr v199b03258d0 = &((Struct_199b031e920*)v199b0325790.raw)[v199b0325b90];
+        float v199b0325910 = ((Struct_199b031e920*)v199b03258d0)->x;
+        float v199b0325450 = v199b0325e90 + v199b0325910;
+        Ptr v199b0325750 = &sum;
+        Ptr v199b0325bd0 = &((Struct_199b031e920*)v199b0325750)->x;
+        *(float*)v199b0325bd0 = v199b0325450;
+        Ptr v199b0325810 = &sum;
+        float v199b0325950 = ((Struct_199b031e920*)v199b0325810)->y;
+        uint64_t v199b0325d10 = i;
+        Slice v199b0325850 = vecs;
+        Ptr v199b0325490 = &((Struct_199b031e920*)v199b0325850.raw)[v199b0325d10];
+        float v199b0325c10 = ((Struct_199b031e920*)v199b0325490)->y;
+        float v199b0325890 = v199b0325950 + v199b0325c10;
+        Ptr v199b0325a10 = &sum;
+        Ptr v199b0325350 = &((Struct_199b031e920*)v199b0325a10)->y;
+        *(float*)v199b0325350 = v199b0325890;
+        Ptr v199b03256d0 = &sum;
+        float v199b0325550 = ((Struct_199b031e920*)v199b03256d0)->z;
+        uint64_t v199b0325ad0 = i;
+        Slice v199b0325650 = vecs;
+        Ptr v199b0325610 = &((Struct_199b031e920*)v199b0325650.raw)[v199b0325ad0];
+        float v199b0325a90 = ((Struct_199b031e920*)v199b0325610)->z;
+        float v199b0325d50 = v199b0325550 + v199b0325a90;
+        Ptr v199b0325dd0 = &sum;
+        Ptr v199b0325d90 = &((Struct_199b031e920*)v199b0325dd0)->z;
+        *(float*)v199b0325d90 = v199b0325d50;
       }
       ++i;
     }
   }
-  Struct_2672449e4e0 v267244a5610 = sum;
-  return v267244a5610;
+  Struct_199b031e920 v199b0325e50 = sum;
+  return v199b0325e50;
 }
 
